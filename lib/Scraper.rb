ea4d63391cdb7a6d2b4fccaf40d24887734486d1
Diego Vasquez <botellongo69@outlook.com>
2020-02-11 17:33:28 +0000

    modified scraper class for nokogiri parsing

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,11 +1,13 @@
 require 'pry'
+require 'nokogiri'
 require 'open-uri'
 
+
 class Scraper
-  def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    apps = []
-    apps
+  def scrape_index_page(index_url)
+   index_page = Nokogiri::HTML(open(index_url))
+   apps = []
+   app_list = index_page.css("div.ZmHEEd").select {|app| app.css()}
   end
 end
 binding.pry
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,5 @@
 require 'pry'
 
 class Command_Line_Interface
-  url = "https://play.google.com/store/apps/top?hl=en_US"
+  url = "https://play.google.com/store/apps/collection/cluster?clp=0g4jCiEKG3RvcHNlbGxpbmdfZnJlZV9BUFBMSUNBVElPThAHGAM%3D:S:ANO1ljKs-KA&gsr=CibSDiMKIQobdG9wc2VsbGluZ19mcmVlX0FQUExJQ0FUSU9OEAcYAw%3D%3D:S:ANO1ljL40zU&hl=en_US"
 end
