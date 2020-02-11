a8ebfdab90526db179617dc0f1e3f9f3a1c31c49
Diego Vasquez <botellongo69@outlook.com>
2020-02-11 19:43:41 +0000

    Modification on Scraper class

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -5,13 +5,15 @@ require 'open-uri'
 
 class Scraper
   def scrape_index_page(index_url)
-   index_page = Nokogiri::HTML(open(index_url))
-   apps = []
-   index_page.css("div.b8cIId.ReQCgd").each do |app|
-     app_name = "#{app.css("div.WsMG1c.nnK0zc").text}"
-     app_developer = "#{app.css("div.KoLSrc").text}"
-     app_description = "#{app.css("div.b8cIId.f5NCO")}"
-   end
+    index_page = Nokogiri::HTML(open(index_url))
+    apps = []
+    index_page.each_with_index do |app, index|
+      app_name = app.css("div.WsMG1c.nnK0zc")[index].text
+      app_developer = app.css("div.KoLSrc")[index].text
+      app_description = app.css("div.b8cIId.f5NCO")[index].text
+      apps << {name: app_name, developer: app_developer, description: app_description}
+    end
+    puts apps
   end
+  binding.pry
 end
-binding.pry
