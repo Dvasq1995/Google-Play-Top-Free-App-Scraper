fb6716d9e3705375fd7bd125a876948b0cf67a20
Diego Vasquez <botellongo69@outlook.com>
2020-02-12 21:56:51 +0000

    Removed console and setup files from bin, replaced with run file

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,13 +3,17 @@ require 'pry'
 class Command_Line_Interface
   url = "https://play.google.com/store/apps/top?hl=en_US"
   
-   def run
+  def run
     make_apps
-    add_attributes_to_apps
   end
-
+  
   def make_apps
-    apps_array = Scraper.scrape_index_page(url)
-    App.create_from_collection(apps_array)
+    Scraper.scrape_index_page(url)
+  end
+  
+  def display_apps
+    App.map.with_index do |app, i|
+      puts "#{i + 1}#{app.name}\n"
+    end
   end
 end
